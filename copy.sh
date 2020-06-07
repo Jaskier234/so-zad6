04ba51d718bc17cca24f4719e7a4178fba287bba
artur234 <jaskier234@gmail.com>
2020-06-07 20:48:02 +0200

    Initial commit

---
+++ copy.sh
@@ -0,0 +1,7 @@
+
+scp dfa.c minix:/usr/src/minix/drivers/dfa/
+scp Makefile minix:/usr/src/minix/drivers/dfa/
+scp ioc_dfa.h minix:/usr/include/sys/
+scp ioc_dfa.h minix:/usr/src/minix/include/sys/
+scp system.conf minix:/etc/
+#scp -r tests minix:/root
