488dbf7d45306b2404e54d07644faa0a061c62a7
artur234 <jaskier234@gmail.com>
2020-06-08 14:27:56 +0200

    Finished

--- dfa.c
+++ dfa.c
@@ -6,14 +6,12 @@
 #include <minix/ioctl.h>
 #include <sys/ioc_dfa.h>
 
-#define HELLO_MESSAGE "Hello, DFA!\n"
-
 const int CHAR_SIZE = 256;
-char current_state;
-char accepting_states[CHAR_SIZE];
-char transition[CHAR_SIZE][CHAR_SIZE];
+unsigned char current_state;
+unsigned char accepting_states[CHAR_SIZE];
+unsigned char transition[CHAR_SIZE][CHAR_SIZE];
 
-const int BUF_SIZE = 4; // TODO Increase after tests
+const int BUF_SIZE = 4000;
 char buffer[BUF_SIZE];
 
 /*
@@ -48,20 +46,8 @@ static ssize_t dfa_read(devminor_t UNUSED(minor), u64_t position,
     endpoint_t endpt, cp_grant_id_t grant, size_t size, int UNUSED(flags),
     cdev_id_t UNUSED(id))
 {
-//    u64_t dev_size;
     char *ptr;
     int ret;
-//    char *buf = HELLO_MESSAGE;
-
-//    printf("hello_read()\n");
-
-    /* This is the total size of our device. */
-//    dev_size = (u64_t) strlen(buf);
-
-    /* Check for EOF, and possibly limit the read size. */
-//    if (position >= dev_size) return 0;		/* EOF */
-//    if (position + size > dev_size)
-//        size = (size_t)(dev_size - position);	/* limit size */
 
     if (accepting_states[current_state] == 1) {
         memset(buffer, 'Y', min(BUF_SIZE, size));
@@ -101,7 +87,8 @@ static ssize_t dfa_write(devminor_t UNUSED(minor), u64_t position,
         
         /* Make transitions */
         for (int i = 0; i < chunk; i++) {
-            current_state = transition[current_state][buffer[i]]; 
+            unsigned char p = buffer[i];
+            current_state = transition[current_state][p]; 
         }
 
         bytes_written += chunk;
@@ -113,7 +100,6 @@ static ssize_t dfa_write(devminor_t UNUSED(minor), u64_t position,
 
 static int sef_cb_lu_state_save(int UNUSED(state)) {
 /* Save the state. */
-//    ds_publish_u32("open_counter", open_counter, DSF_OVERWRITE);
     int rc;
     rc = ds_publish_mem("dfa_current_state", &current_state, 1, DSF_OVERWRITE);
     if (rc != OK) return rc;
@@ -147,9 +133,6 @@ static int lu_state_restore() {
     if (rc != OK) return rc; 
     rc = ds_delete_mem("dfa_transition");
     if (rc != OK) return rc; 
-//    ds_retrieve_u32("open_counter", &value);
-//    ds_delete_u32("open_counter");
-//    open_counter = (int) value;
 
     return OK;
 }
@@ -168,7 +151,10 @@ static int dfa_ioctl(devminor_t minor, unsigned long request, endpoint_t endpt,
     case DFAIOCADD: 
         rc = sys_safecopyfrom(endpt, grant, 0, (vir_bytes) buf, 3);
         if (rc == OK) {
-            transition[buf[0]][buf[1]] = buf[2];
+            unsigned char p = buf[0];
+            unsigned char a = buf[1];
+            unsigned char q = buf[2];
+            transition[p][a] = q;
             current_state = 0;
         }
         break;
@@ -176,16 +162,16 @@ static int dfa_ioctl(devminor_t minor, unsigned long request, endpoint_t endpt,
     case DFAIOCACCEPT: 
         rc = sys_safecopyfrom(endpt, grant, 0, (vir_bytes) buf, 1);
         if (rc == OK) {
-            accepting_states[buf[0]] = 1;
-            current_state = 0;
+            unsigned char p = buf[0];
+            accepting_states[p] = 1;
         }
         break;
 
     case DFAIOCREJECT: 
         rc = sys_safecopyfrom(endpt, grant, 0, (vir_bytes) buf, 1);
         if (rc == OK) {
-            accepting_states[buf[0]] = 0;
-            current_state = 0;
+            unsigned char p = buf[0];
+            accepting_states[p] = 0;
         }
         break;
 
@@ -231,19 +217,15 @@ static int sef_cb_init(int type, sef_init_info_t *UNUSED(info))
 
     switch(type) {
         case SEF_INIT_FRESH:
-            printf("%s", HELLO_MESSAGE);
         break;
 
         case SEF_INIT_LU:
             /* Restore the state. */
             lu_state_restore();
             do_announce_driver = FALSE;
-
-            printf("%sHey, I'm a new version!\n", HELLO_MESSAGE);
         break;
 
         case SEF_INIT_RESTART:
-            printf("%sHey, I've just been restarted!\n", HELLO_MESSAGE);
         break;
     }
 
